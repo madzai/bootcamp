0f007cbbc7a81d97444cbcaa7c711e16b179c490
madzai <yw.edwin@gmail.com>
2025-12-01 17:57:20 +0800

    update

---
+++ demo-helloworld/src/Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+  public static void main(String[] args) {
+    // Tools
+    // 1. System.out.println("abc");
+    // Every line of java code must end with ;
+    System.out.println("Hello world!");
+  }
+}
