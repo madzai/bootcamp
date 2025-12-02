740d9a89253b776af23b6b70873433b9e4218ecc
madzai <yw.edwin@gmail.com>
2025-12-02 11:00:28 +0800

    added goodbye files

---
+++ demo-helloworld/src/Goodbye.java
@@ -0,0 +1,7 @@
+public class Goodbye {
+  public static void main(String[] args) {
+    System.out.println("Goodbye");
+    System.out.println("Bye");
+    System.out.println("See you later");
+  }
+}
