ab5003beffcc0feb6c36645dd86122497d0da694
madzai <yw.edwin@gmail.com>
2025-12-08 16:14:11 +0800

    Q7

--- exercises/JavaQuest7.java
+++ exercises/JavaQuest7.java
@@ -9,11 +9,20 @@ public class JavaQuest7 {
 
     // Declare a int array, with value 1 to 10
     // code here
+    int[] integers = new int[10];
+    for (int i = 0; i < 10; i++) {
+      integers[i] = i + 1;
+      System.out.print(i + 1 + " ");
+    }
+
     int sum = 0;
 
     // Write a loop to sum up all value in the int array
     // code here ...
-    System.out.println("The sum is " + sum);
+    for (int i = 0; i < 10; i++) {
+      sum += integers[i];
+    }
+    System.out.println("has sum " + sum);
 
   }
 }
