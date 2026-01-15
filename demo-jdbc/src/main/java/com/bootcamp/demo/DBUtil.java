ca898970369a238be703d59efdb082fae6a4f613
madzai <102968841+madzai@users.noreply.github.com>
2026-01-15 19:16:42 +0800

    update

--- demo-jdbc/src/main/java/com/bootcamp/demo/DBUtil.java
+++ demo-jdbc/src/main/java/com/bootcamp/demo/DBUtil.java
@@ -5,10 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-  // private static final String URL = "jdbc:mysql://localhost:3306/bootcamp_db";
-  private static final String URL = "jdbc:mysql://127.0.0.1:3306/bootcamp_db";
+  private static final String URL = "jdbc:mysql://localhost:3306/bootcamp_db";
   private static final String USER = "root";
-  private static final String PASSWORD = "1408!erkBF"; // 1408!erkBF
+  private static final String PASSWORD = "admin1234";
 
   public static Connection getConnection() throws SQLException {
     return DriverManager.getConnection(URL, USER, PASSWORD);
